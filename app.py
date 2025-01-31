1daadf0fb557a4af3bb7fa34bc01c071392c1d9f
RomanKharkovskoy <roman.kharkovskoy@yandex.ru>
2025-01-31 21:01:34 +0300

    Implement asynchronous result generation and enhance error handling in query processing

--- app.py
+++ app.py
@@ -1,8 +1,22 @@
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
-from funcs import get_result
+import asyncio
+from src.tools.response import ResponseAgent
+from src.tools.search import SearchAgent
+from src.utils.parser import parse_to_json
 
 app = FastAPI()
+
+
+def get_result(id, query):
+    search_agent = SearchAgent()
+    context = asyncio.run(search_agent.search(query))
+
+    response_agent = ResponseAgent()
+    result = asyncio.run(response_agent.generate_response(
+        question=query, context=context))
+
+    return parse_to_json(id, result)
 
 
 class QueryRequest(BaseModel):
@@ -15,8 +29,8 @@ def process_query(request: QueryRequest):
     query_text = request.query
     request_id = request.id
     try:
-        response = get_result(query=query_text)
+        response = get_result(id=request_id, query=query_text)
     except Exception as e:
-        raise HTTPException(status_code=500, detail=f"Ошибка OpenAI: {e}")
+        raise HTTPException(status_code=500, detail=f"Ошибка: {e}")
 
     return response
